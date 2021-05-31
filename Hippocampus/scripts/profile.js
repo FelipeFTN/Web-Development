7a9b87dd3a049913d7511d48c260b7e224f32de5
FelipeFTN <felipe.tenorio123456@gmail.com>
2021-05-31 16:27:53 -0300

    Add Courses Function

--- Hippocampus/scripts/profile.js
+++ Hippocampus/scripts/profile.js
@@ -1,46 +1,16 @@
 //TABLE INVISIBLE
 let table = document.getElementsByClassName("schedule-table")[0];
 table.style.display = "none";
-
-let inputCourses = document.getElementById("courses-input");
-let colorButtton = document.getElementById("color-btn");
+//ADD COURSES
+let input = document.getElementById("courses-input");
+let createBtn = document.getElementById("create-btn");
 let ul = document.getElementById("courses");
 
-
-
-
-/*
-let button = document.getElementById("item-button");
-let itemButtons = document.getElementsByClassName("remove");
-
-function criarElemento() {
-  let li = document.createElement("li");
-
-  let xButton = document.createElement("button");
-  xButton.innerHTML = "X";
-  xButton.className = "remove";
-
-  li.appendChild(xButton);
-
-  let text = document.createTextNode(input.value);
-  li.appendChild(text);
-
-  ul.appendChild(li);
-
-  input.value = "";
-  buttonEvents();
+function createElement(){
+    let inputText = document.createTextNode(input.value);
+    let li = document.createElement("li");
+    li.appendChild(inputText);
+    ul.appendChild(li);
+    input.value = "";
 }
-
-function deleteItem() {
-  this.parentElement.remove();
-}
-
-function buttonEvents() {
-  for (let i = 0; i < itemButtons.length; i++) {
-    itemButtons[i].addEventListener("click", deleteItem);
-  }
-}
-
-button.addEventListener("click", criarElemento);
-buttonEvents();
-*/
+createBtn.addEventListener("click", createElement);
