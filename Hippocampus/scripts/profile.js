a24eaa5ba3aeaa04759a393e9d61563314599c3b
FelipeFTN <felipe.tenorio123456@gmail.com>
2021-06-04 19:47:37 -0300

    Add Courses to Table

--- Hippocampus/scripts/profile.js
+++ Hippocampus/scripts/profile.js
@@ -7,8 +7,10 @@ let createBtn = document.getElementById("create-btn");
 let ul = document.getElementById("courses");
 let colorBtn = document.getElementById('color-btn');
 let color = 0;
+let confirmBtn = document.getElementById("confirm");
 let courses = [];
-const colors = ['#4ECCA3', 'Red', 'Blue', 'Green', 'Purple', 'Orange'];
+const colors = ['#4ECCA3', '#bd3535', '#4456bd', '#18ad1b', '#6d3ebd', '#bd733e'];
+
 function changeColor() {
     color += 1;
     if(color >= colors.length){
@@ -16,7 +18,7 @@ function changeColor() {
     }
     colorBtn.style.backgroundColor = colors[color];
 }
-//CREATE CURSES LI
+//CREATE COURSES LI
 function createElement(){
     if (input.value.trim() != "") {
         let inputText = document.createTextNode(input.value);
@@ -28,10 +30,13 @@ function createElement(){
         ul.appendChild(li);
         input.value = "";
         li.addEventListener("click",  deleteLi);
+        li.addEventListener("click", confirmColor);
+        confirmColor();
     } else {
         return false;
     }
 }
+//DELETE COURSES LI
 function deleteLi(){
     for(let i = 0; i < courses.length; i++){
         if(courses[i] == this.innerText){
@@ -49,9 +54,32 @@ document.addEventListener('keydown', (event) => {
         createElement();
     }
 });
+function confirmColor(){
+    if(courses.length > 0){
+        confirmBtn.style.backgroundColor = "#4ECCA3";
+        confirmBtn.style.color = "White";
+    }else{
+        confirmBtn.style.backgroundColor = "#5e8555";
+        confirmBtn.style.color = "#404040";
+    }
+}
 createBtn.addEventListener("click", createElement);
 colorBtn.addEventListener("click", changeColor);
+confirmBtn.addEventListener("click", confirmPressed);
 //IMPLEMENT ON TABLE
 let pause = 5.00;
 let initialTime = 12.00;
 let finalTime = 18.00;
+let tableContent = ['time', 'sunday', 'monday', 'thuesday', 'wednessday', 'thursday', 'saturday'];
+
+function confirmPressed(){
+    table.style = null;
+    for(let i = 1; i < 6; i+=2){
+        let addPause = tableContent[1]+i;
+        document.getElementById(addPause).innerHTML = "Pausa";
+    }
+    for(let i = 0; i < 6; i+=2){
+        let addCourses = tableContent[1]+i;
+        document.getElementById(addCourses).innerHTML = courses[courses.length];
+    }
+}
