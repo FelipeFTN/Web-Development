67eee05726ba344a7fcee9e70ed93ab4dece6bea
FelipeFTN <felipe.tenorio123456@gmail.com>
2021-06-09 23:51:02 -0300

    Front end Added

--- Hippocampus/scripts/profile.js
+++ Hippocampus/scripts/profile.js
@@ -54,27 +54,28 @@ document.addEventListener('keydown', (event) => {
         createElement();
     }
 });
+
 function confirmColor(){
-    if(courses.length > 0){
+    if(courses.length > 2){
         confirmBtn.style.backgroundColor = "#4ECCA3";
         confirmBtn.style.color = "White";
+        confirmBtn.addEventListener("click", confirmPressed);
     }else{
         confirmBtn.style.backgroundColor = "#5e8555";
         confirmBtn.style.color = "#404040";
+        table.style.display = "hidden";
     }
 }
 createBtn.addEventListener("click", createElement);
 colorBtn.addEventListener("click", changeColor);
-confirmBtn.addEventListener("click", confirmPressed);
 //IMPLEMENT ON TABLE
 let pause = 5.00;
-let initialTime = 12.00;
-let finalTime = 18.00;
 let tableContent = ['time', 'sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 
 function confirmPressed(){
-    table.style = null;
+    table.style.display = null;
     let x = 0;
+    //FILL DATES
     for(let i = 0; i < 6; i+=2){
         if(x >= courses.length){
             x = 0;
@@ -179,5 +180,30 @@ function confirmPressed(){
         if(sundayElement.innerHTML == ""){
             sundayElement.innerHTML = "Pausa";
         }
+    }
+    //FILL TIME
+    let initialSchedule = document.getElementById("initialTime");
+    let intervalSchedule = document.getElementById("intervalTime");
+    let initialTime = initialSchedule.value.split(":");
+    let intervalTime = intervalSchedule.value.split(":");
+    let nextTime = parseInt(initialTime[0], 10);
+    let minutes = parseInt(initialTime[1], 10);
+    if(minutes >= 60){
+        minutes -= 60;
+        nextTime += 1;
+    }
+    
+    for(let i = 0; i < 6; i++){
+        if(minutes >= 60){
+            minutes -= 60;
+            nextTime += 1;
+        }
+        let timeNumber = tableContent[0]+i;
+        document.getElementById(timeNumber).innerHTML = nextTime+":"+ minutes;
+        if(minutes.toString().length <= 1){
+            document.getElementById(timeNumber).innerHTML = nextTime+":"+minutes+"0";
+        }
+        nextTime += parseInt(intervalTime[0], 10);
+        minutes += parseInt(intervalTime[1], 10);
     }
 }
