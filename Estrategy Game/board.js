e092e3e0b3061cef8879b728e10aaa4db8f535d2
FelipeFTN <felipe.tenorio123456@gmail.com>
2021-09-14 21:30:42 -0300

    Mouse Hover Place

--- Estrategy Game/board.js
+++ Estrategy Game/board.js
@@ -1,4 +1,5 @@
 function table() {
+    ctx.clearRect(0, 0, 500, 500)
     for (let i = 50; i < 500; i+=50){
         // Vertical Grid
         ctx.strokeStyle = "Gray"
@@ -7,15 +8,17 @@ function table() {
         ctx.moveTo(i, 0);
         ctx.lineTo(i, 500);
         ctx.stroke();
-    
-    // Horizontal Grid
+        // Horizontal Grid
         ctx.beginPath();
         ctx.setLineDash([2, ]);
         ctx.moveTo(0, i);
         ctx.lineTo(500, i);
         ctx.stroke();
-        
     }
+    // Mouse Block Selected
+    ctx.beginPath()
+    ctx.fillStyle = 'Orange'
+    ctx.fillRect(blockSelected[0], blockSelected[1], 50, 50)
     // Kingdom area
     ctx.beginPath()
     ctx.fillStyle = 'Gray'
--- Estrategy Game/mouse.js
+++ Estrategy Game/mouse.js
@@ -1,11 +1,23 @@
+let blockSelected = []
 
 function showCoords(event) {
-  var x = event.clientX;
-  var y = event.clientY;
-  var coor = "X coords: " + x + ", Y coords: " + y;
-  document.getElementById("demo").innerHTML = coor;
+    let x = event.clientX - 10;
+    let y = event.clientY - 10;
+    let coor = "X coords: " + x + ", Y coords: " + y;
+    document.getElementById("demo").innerHTML = coor;
+    mouseListening(x, y)
 }
 
 function clearCoords() {
-  document.getElementById("demo").innerHTML = "";
+    document.getElementById("demo").innerHTML = "";
+}
+// Mouse Listening Hover
+function mouseListening(x, y){
+    for(let i = 0; i < 500; i+=50){
+        for(let j = 0; j < 500; j+=50){
+            if(x >= i && x <= i+50 && y >= j && y <= j+50){
+                blockSelected = [i, j]
+            }
+        }
+    }
 }
