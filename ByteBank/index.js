9d5277f0dcbed8fee2e7a5d1ceae1990c7cf7fa6
FelipeFTN <felipe.tenorio123456@gmail.com>
2021-09-17 13:49:04 -0300

    Custommer Improve

--- ByteBank/index.js
+++ ByteBank/index.js
@@ -1,25 +1,22 @@
 import { Custommer } from "./Custommer.js"
 import { CheckingAccount } from "./CheckingAccount.js"
-const custommer1 = new Custommer()
-
-custommer1.name = "Ricardo"
-custommer1.cpf = 11122233309
-custommer1.rg = 123456789
 
 const ricardosCheckingAccount = new CheckingAccount()
+ricardosCheckingAccount.custommer = new Custommer()
+ricardosCheckingAccount.custommer.name = "Ricardo"
+ricardosCheckingAccount.custommer.cpf = 11122233344
+ricardosCheckingAccount.custommer.rg = 123456789
 ricardosCheckingAccount.agency = 1001
 ricardosCheckingAccount.balance = 500
-ricardosCheckingAccount.custommer = custommer1
-
-const custommer2 = new Custommer()
-custommer2.name = "Alice"
-custommer2.cpf = 88899910101 
-custommer2.rg = 123456788
 
 const alicesCheckingAccount = new CheckingAccount()
-alicesCheckingAccount.custommer = custommer2
+alicesCheckingAccount.custommer = new Custommer()
+alicesCheckingAccount.custommer.name = "Alice"
+alicesCheckingAccount.custommer.cpf = 88899910101
+alicesCheckingAccount.custommer.rg = 123456788
 alicesCheckingAccount.agency = 1002
 
 ricardosCheckingAccount.transfer(500, alicesCheckingAccount)
 
 console.log(alicesCheckingAccount)
+console.log(ricardosCheckingAccount)
