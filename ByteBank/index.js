1d65661408e74f31938b49f1529f842e91054c3c
FelipeFTN <felipe.tenorio123456@gmail.com>
2021-09-17 15:33:38 -0300

    Getters and Setters

--- ByteBank/index.js
+++ ByteBank/index.js
@@ -1,22 +1,24 @@
 import { Custommer } from "./Custommer.js"
 import { CheckingAccount } from "./CheckingAccount.js"
 
+const custommer1 = new Custommer()
+custommer1.name = "Ricardo"
+custommer1.cpf = 11122233309
+
+const custommer2 = new Custommer()
+custommer2.name = "Alice"
+custommer2.cpf = 88822233309
+
 const ricardosCheckingAccount = new CheckingAccount()
-ricardosCheckingAccount.custommer = new Custommer()
-ricardosCheckingAccount.custommer.name = "Ricardo"
-ricardosCheckingAccount.custommer.cpf = 11122233344
-ricardosCheckingAccount.custommer.rg = 123456789
 ricardosCheckingAccount.agency = 1001
-ricardosCheckingAccount.balance = 500
+ricardosCheckingAccount.custommer = custommer1
+ricardosCheckingAccount.deposit(500)
 
 const alicesCheckingAccount = new CheckingAccount()
-alicesCheckingAccount.custommer = new Custommer()
-alicesCheckingAccount.custommer.name = "Alice"
-alicesCheckingAccount.custommer.cpf = 88899910101
-alicesCheckingAccount.custommer.rg = 123456788
+alicesCheckingAccount.custommer = custommer2
 alicesCheckingAccount.agency = 1002
 
-ricardosCheckingAccount.transfer(500, alicesCheckingAccount)
+let value = 200
+ricardosCheckingAccount.transfer(value, alicesCheckingAccount)
 
-console.log(alicesCheckingAccount)
-console.log(ricardosCheckingAccount)
+console.log(ricardosCheckingAccount.custommer)
