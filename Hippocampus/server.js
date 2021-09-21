e8a61d6d82989ff024d8f2265a922030843a3f1e
FelipeFTN <felipe.tenorio123456@gmail.com>
2021-09-21 14:59:03 -0300

    Get Form Data

--- Hippocampus/server.js
+++ Hippocampus/server.js
@@ -18,10 +18,16 @@ function article(req, res){
 function community(req, res){
     res.sendFile(__dirname + '/static/community.html')
 }
+app.use(express.urlencoded());
+app.use(express.json());
+function loginCheck(req, res){
+    res.send('Recebido'+ req.body.name)
+}
 app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', index)
 app.get('/login', login)
+app.post('/loginCheck', loginCheck)
 app.get('/plan', plan)
 app.get('/articles', article)
 app.get('/community', community)
