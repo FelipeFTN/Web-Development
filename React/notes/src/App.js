7919fca48df03a70492767c7e549ed5da308a03e
FelipeFTN <felipe.tenorio123456@gmail.com>
2021-09-22 16:40:51 -0300

    Design Pattern And Plugins

--- React/notes/src/App.js
+++ React/notes/src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NoteList } from './components/Notes'
-function App() {
-  return (
-    <section>
-    <form>
-      <input type="text" name="title" id="titleField" placeholder="Titulo"/>
-      <textarea name="message" id="messageField" cols="30" rows="10" placeholder="Mensagem">
-      </textarea>
-      <button>Criar Nota</button>
-    </form>
-    <NoteList/>
-    </section>
-  );
-}
-export default App;
+import { SignupForm } from './components/SignupForm';
+
+class App extends Component {
+  render(){
+    return (
+      <section>
+      <SignupForm />
+      <NoteList />
+      </section>
+      );
+    }
+  }
+export default App;  
--- React/notes/src/components/Notes.jsx
+++ React/notes/src/components/Notes.jsx
@@ -1,24 +1,18 @@
 import React, { Component } from "react"
+import CreateNotes from "./CreateNotes"
 
 export class NoteList extends Component {
     render(){
         return(
             <ul>
                 <li>
-                    <section>
-                        <header>
-                            <h3>Titulo</h3>
-                        </header>
-                        <p>Escreva sua rota</p>
-                    </section>
+                    <CreateNotes/>
                 </li>
                 <li>
-                <section>
-                    <header>
-                        <h3>Titulo</h3>
-                    </header>
-                        <p>Escreva sua nota</p>
-                </section>
+                    <CreateNotes />
+                </li>
+                <li>
+                    <CreateNotes />
                 </li>
             </ul>
         )
