12ea422a38d4fb297abdee85a1cb800e909c27ce
FelipeFTN <felipe.tenorio123456@gmail.com>
2021-09-22 18:23:22 -0300

    Login and Register

--- Hippocampus/server.js
+++ Hippocampus/server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const path = require('path')
+const { send } = require('process')
 const app = express()
+const main = require('./app.js')
+
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'static')));
 
 // Routes
 function index(req, res){
@@ -18,17 +24,33 @@ function article(req, res){
 function community(req, res){
     res.sendFile(__dirname + '/static/community.html')
 }
-app.use(express.urlencoded());
-app.use(express.json());
-function loginCheck(req, res){
-    res.send('Recebido'+ req.body.name)
-}
-app.use(express.static(path.join(__dirname, 'static')));
+function register(req, res){
+    try{
+        let foundUser = users.find((data) => req.body.email === data.email)
+        if(!foundUser){
+            let hashPassword = await bcrypt.hash(req.body.password, 10)
+            let newUser = { 
+                id: Date.now(),
+                username: req.body.username,
+                email: req.body.email,
+                password: hashPassword,
+            }
+            users.push(newUser)
+            console.log('User list: ', users)
 
-app.get('/', index)
+            res.send("<div align ='center'><h2>Registration successful</h2></div><br><br><div align='center'><a href='./login.html'>login</a></div><br><br><div align='center'><a href='./registration.html'>Register another user</a></div>")
+        }else{
+            res.send("<div align ='center'><h2>Email already used</h2></div><br><br><div align='center'><a href='./registration.html'>Register again</a></div>")
+        }
+    } catch {
+        res.send("Internal Server Error")
+    }
+}
+
 app.get('/login', login)
-app.post('/loginCheck', loginCheck)
 app.get('/plan', plan)
 app.get('/articles', article)
 app.get('/community', community)
+app.post('/register', register)
+app.get('/*', index)
 app.listen(3000)
