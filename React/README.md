fb89e79df49135616b05b7730b2c805ef15663cf
FelipeFTN <felipe.tenorio123456@gmail.com>
2021-09-22 13:57:02 -0300

    React Project

---
+++ React/README.md
@@ -0,0 +1,7 @@
+# Introduction
+
+This Repository was made for knowledge purposes. It serves me mainly to understand how the React works and how to work with it.
+
+# React
+
+There is no relevant project until now.
---
+++ React/notes/src/components/Notes.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react"
+
+export class NoteList extends Component {
+    render(){
+        return(
+            <ul>
+                <li>
+                    <section>
+                        <header>
+                            <h3>Titulo</h3>
+                        </header>
+                        <p>Escreva sua rota</p>
+                    </section>
+                </li>
+                <li>
+                <section>
+                    <header>
+                        <h3>Titulo</h3>
+                    </header>
+                        <p>Escreva sua nota</p>
+                </section>
+                </li>
+            </ul>
+        )
+    }
+}
