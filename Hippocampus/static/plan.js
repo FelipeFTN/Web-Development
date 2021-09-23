02c090462e5b4a6a42dd449193dc7a1bc749eeaf
FelipeFTN <felipe.tenorio123456@gmail.com>
2021-09-23 10:34:26 -0300

    Planning Time Working

--- Hippocampus/static/plan.js
+++ Hippocampus/static/plan.js
@@ -1,10 +1,22 @@
-const initialTime = parseFloat('10.00')
-const finalTime = parseFloat('16.00')
+const initialTime = '13.00'
+const finalTime = '19.00'
 let timeI = initialTime
 let timeF
+let timeIF
+let timeS
 
-while(timeI <= finalTime){
-    console.log(timeI+ " - "+(timeI = parseFloat(timeI + parseFloat('00.25'))))
-    console.log()
-
+while(parseFloat(timeI) <= parseFloat(finalTime) || parseFloat(timeIF) <= parseFloat(finalTime)){
+    timeIF = parseFloat(timeI) + parseFloat('00.25')
+    timeS = String(timeIF).split('.')
+    console.log(timeI+ " - "+(timeS[0]+'.'+timeS[1]))
+    timeS[1] = parseFloat(timeS[1]) + parseFloat('5')
+    if(parseFloat(timeS[1]) > parseFloat('59')){
+        timeS[0] = parseFloat(timeS[0]) + parseFloat('1')
+        timeS[1] = parseFloat(timeS[1]) - parseFloat('60')
+    }
+    timeI = timeS[0]+'.'+timeS[1]
+    if(timeS[0] == parseFloat(finalTime.split('.')[0])){
+        timeS[1] = parseFloat('0')
+        break
+    }
 }
