573f6313cbf73708bedb35f69e774d730f6d6bf0
FelipeFTN <felipe.tenorio123456@gmail.com>
2021-09-24 21:26:40 -0300

    React + NodeJs Login and Register working!

--- Hippocampus/client/src/App.js
+++ Hippocampus/client/src/App.js
@@ -8,8 +8,15 @@ function App() {
   const [usernameReg, setUsernameReg] = useState('')
   const [passwordReg, setPasswordReg] = useState('')
 
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
   const register = () => {
     Axios.post('http://localhost:3001/register', {userEmail: userEmailReg, username: usernameReg, password: passwordReg,}).then((response) => {console.log(response)})
+  }
+
+  const login = () => {
+    Axios.post('http://localhost:3001/login', {username: username, password: password}).then((response) => {console.log(response)})
   }
 
   return (
@@ -32,11 +39,16 @@ function App() {
         }/>
         <button onClick={register}>Register</button>
       </div>
+      <br/>
       <div className="login">
         <h1>Login</h1>
-        <input type="text" placeholder="Login" />
-        <input type="password" placeholder="Password" />
-        <button>Login</button>
+        <input type="text" placeholder="Login" onChange={(e)=>{
+          setUsername(e.target.value)
+        }} />
+        <input type="password" placeholder="Password" onChange={(e)=>{
+          setPassword(e.target.value)
+        }}/>
+        <button onClick={login}>Login</button>
       </div>
     </div>
   );
--- Hippocampus/server/index.js
+++ Hippocampus/server/index.js
@@ -10,7 +10,7 @@ app.use(cors())
 const db = mysql.createConnection({
     user: "root",
     host: "localhost",
-    password: "hippocampus",
+    password: "secret",
     database: "hippocampus",
 })
 
@@ -20,9 +20,27 @@ app.post('/register', (req, res) =>{
     const username = req.body.username
     const password = req.body.password
 
-    db.query("INSERT INTO users (user_name, user_password, salt_password, user_email) VALUES (?, ?, ?, ?)", [username, password, salt, userEmail],
+    db.query("INSERT INTO users (username, password, email) VALUES (?, ?, ?)", [username, password, userEmail],
     (err, result) => {
         console.log(err)
+    })
+})
+
+app.post('/login', (req, res) =>{
+    const userEmail = req.body.userEmail
+    const username = req.body.username
+    const password = req.body.password
+
+    db.query("SELECT * FROM users WHERE username = ? AND password = ?", [username, password],
+    (err, result) => {
+        if(err){
+            res.send({err: err})
+        }
+            if(result.length > 0){
+                res.send(result)
+            }else{
+                res.send({ message: "Wrong username or password!" })
+            }
     })
 })
 
