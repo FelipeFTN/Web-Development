068ba1ab96dd4f5275a2e51ce44f3c9d96ba62ab
FelipeFTN <felipe.tenorio123456@gmail.com>
2021-10-20 21:29:58 -0300

    CreatePlan Improved

--- Hippocampus/client/src/pages/create-plan/CreatePlan.js
+++ Hippocampus/client/src/pages/create-plan/CreatePlan.js
@@ -1,54 +1,66 @@
 import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import './CreatePlan.css'
 
 function CreatePlan(){
-
-    // const initialTime = '13.00'
-    // const finalTime = '19.00'
-    // let timeI = initialTime
-    // let timeF
-    // let timeS
-
-    // while(parseFloat(timeI) <= parseFloat(finalTime) || parseFloat(timeF) <= parseFloat(finalTime)){
-    //     timeF = parseFloat(timeI) + parseFloat('00.25')
-    //     timeS = String(timeF).split('.')
-    //     console.log(timeI+ " - "+(timeS[0]+'.'+timeS[1]))
-    //     timeS[1] = parseFloat(timeS[1]) + parseFloat('5')
-    //     if(parseFloat(timeS[1]) > parseFloat('59')){
-    //         timeS[0] = parseFloat(timeS[0]) + parseFloat('1')
-    //         timeS[1] = parseFloat(timeS[1]) - parseFloat('60')
-    //     }
-    //     timeI = timeS[0]+'.'+timeS[1]
-    //     if(timeS[0] == parseFloat(finalTime.split('.')[0])){
-    //         timeS[1] = parseFloat('0')
-    //         break
-    //     }
-    // }
-
-    let subjects = []
-
+    const [subject, setSubject] = useState('');
+    const [subjects, setSubjects] = useState([]);
+    const [time, setTime] = useState({initialTime:"", finalTime:""});
     const handleAddSubject = () => {
-        if(subject != ''){
-            subjects.push(subject)
+        if(subject !== ''){
+            setSubjects([...subjects, subject]);
             console.log(subjects)
         }
     }
+    const handleRemove = () => {
+    }
+    const handleSubmit = () => {
+        if(time.initialTime !== "" && time.finalTime !== ""){
 
-    const [subject, setSubject] = useState('');
-    console.log(subject)
+            const initialTime = time.initialTime
+            const finalTime = time.finalTime
+            let timeI = initialTime
+            let timeF
+            let timeS
+            
+            while(parseFloat(timeI) <= parseFloat(finalTime) || parseFloat(timeF) <= parseFloat(finalTime)){
+                timeF = parseFloat(timeI) + parseFloat('00.25')
+                timeS = String(timeF).split('.')
+                console.log(timeI+ " - "+(timeS[0]+'.'+timeS[1]))
+                timeS[1] = parseFloat(timeS[1]) + parseFloat('5')
+                if(parseFloat(timeS[1]) > parseFloat('59')){
+                    timeS[0] = parseFloat(timeS[0]) + parseFloat('1')
+                    timeS[1] = parseFloat(timeS[1]) - parseFloat('60')
+                }
+                timeI = timeS[0]+'.'+timeS[1]
+                if(timeS[0] === parseFloat(finalTime.split('.')[0])){
+                    timeS[1] = parseFloat('0')
+                    break
+                }
+            }
+            
+            console.log(time.initialTime+" "+time.finalTime)
+        }else{
+            alert("Informe os Horarios!")
+        }
+    }
 
     return(
-        <div className="CreatePlan">
+        <div className="CreatePlan" align="center">
             <Navbar/>
             <h1>Criar Plano</h1>
             <input type="text" className="Materia" placeholder="Materia" onChange={(e) => { setSubject(e.target.value)}}/>
             <button className="btn btn-primary" onClick={handleAddSubject}>Adicionar</button>
-            // FAZER MAP DO SUBJECTS
             <br />
-            <input type="text" className="InitialTime" placeholder="Horario Inicial"/>
-            <input type="text" className="FinalTime" placeholder="Horario Final"/>
+            <ul>
+                {subjects.map((item, index) =>{
+                    return <li key={index} id={"subject"+index} onClick={handleRemove}>{item}</li>
+                })}
+            </ul>
+            <input type="time" className="InitialTime" placeholder="Horario Inicial" onChange={(e) => setTime({ ...time, initialTime: e.target.value})} required/>
+            <input type="time" className="FinalTime" placeholder="Horario Final" onChange={(e) => setTime({ ...time, finalTime: e.target.value})} required/>
             <br />
-            <button className="btn btn-primary">Gerar plano de estudos</button>
+            <button className="btn btn-primary" onClick={handleSubmit}>Gerar plano de estudos</button>
         </div>
     )
 }
