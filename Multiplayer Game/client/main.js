26b661f651897bd3470269dd63cc026ac76bb67a
FelipeFTN <felipe.tenorio123456@gmail.com>
2022-09-12 23:30:02 -0300

    📨 sending events to server

--- Multiplayer Game/client/main.js
+++ Multiplayer Game/client/main.js
@@ -1,6 +1,11 @@
 import { io } from "socket.io-client";
 
 const socket = io("http://localhost:3000");
+
+// When connect with server
 socket.on('connect', () => {
     document.write(`<h1>Your are connected to ${socket.id}</h1>`);
 });
+
+// Send an event to server
+socket.emit('custom-event', 10, 'Hi', { a: 'a' });
--- Multiplayer Game/server/server.js
+++ Multiplayer Game/server/server.js
@@ -6,4 +6,10 @@ const io = require('socket.io')(3000, {
 
 io.on('connection', socket => {
     console.log(`socket id: ${socket.id}`);
+
+    // When receive a event from client
+    socket.on("custom-event", (number, string, object) => {
+        console.log(number, string, object);
+    });
+
 });
